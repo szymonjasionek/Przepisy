a811aacb3f7a8ca914939efe1c4aa6b0d93f363c
szymonjasionek <szymon.jasionek@onet.pl>
2024-06-19 19:57:19 +0200

    First

---
+++ przepisy/src/Header.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function ButtonAppBar(props) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: props.bgColor }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Wyszukiwarka przepisów<br></br>
+            Znajdź swój przepis
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ przepisy/src/Searchbar.js
@@ -0,0 +1,46 @@
+import { TextField } from "@mui/material";
+import { InputAdornment } from "@mui/material";
+import { IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+
+export const Searchbar = ({ setRecipes }) => {
+  const [query, setQuery] = useState("chicken");
+  console.log("Query:", query);
+
+  async function handleClick() {
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}`
+      );
+
+      const data = await response.json();
+      console.log("Data from API:", data);
+      setRecipes(data.hits || []);
+    } catch (error) {
+      console.error("Fetch error: ", error);
+      setRecipes([]);
+    }
+  }
+
+  function handleChange(event) {
+    setQuery(event.target.value);
+  }
+
+  return (
+    <TextField
+      label="Wyszukaj"
+      onChange={handleChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton onClick={handleClick}>
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+      fullWidth
+    />
+  );
+};
